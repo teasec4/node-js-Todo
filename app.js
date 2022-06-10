fde1138cc66751f59d76ceed21288bdd7b8d19ef
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-06-10 17:59:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const todoController = require('./controllers/todoController')
 
 const app = express()
 
+const PORT = process.env.PORT || 80
+
+DATABASE_URL='postgres://localhost/foobar'
+HTTP_TIMEOUT=10000
+
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
 
 todoController(app)
 
-app.listen(3000, () => {
-    console.log('Server is listening 3000')
+app.listen(PORT, () => {
+    console.log('Server is listening 80')
 })
